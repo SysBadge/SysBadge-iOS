657a0312e973fba8c0d87853ea1af0f930c875b6
Finn Behrens <me@kloenk.dev>
2023-10-20 16:09:06 +0200

    Initial commit

---
+++ SysBadge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SysBadge
+//
+//  Created by Finn Behrens on 20.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SysBadge/SysBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SysBadgeApp.swift
+//  SysBadge
+//
+//  Created by Finn Behrens on 20.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SysBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
